1434e00dad6321b4fd9ccdc612c10f030ecf2ef2
Thomas Violent <thomasviolent@gmail.com>
2021-11-25 17:54:19 +0100

    Add: Comment to server function in header file

--- src/main.c
+++ src/main.c
@@ -1,14 +1,25 @@
-#include <stdio.h>		
+#include <stdio.h>
 #include <stdlib.h>
 #include "net_prisoner.h"
 
+void *trahir(int client)
+{
+
+}
+
+void *cooperer(int client)
+{
+
+}
+
 int main()
-{	
-  net_dbg("yolo");
-	net_server_init();
+{
+	net_dbg("yolo");
+	net_server_init("0.0.0.0", 7799);
+	net_server_set_trahir(*trahir);
 
 	// création de la connexion
-	char * addrServer = "127.0.0.1\0";  
+	char *addrServer = "127.0.0.1\0";
 	net_client_connexion(addrServer, 7799);
 
 	// envoie d'un message
@@ -18,7 +29,7 @@ int main()
 	write(net_client_sockfd, msg, strlen(msg));
 
 	// écoute d'une réponse
-    memset(msg,'\0', MSGLENGHT);
+	memset(msg, '\0', MSGLENGHT);
 	net_thread_process(&msg);
 	printf("received : %s\n", msg);
 
--- src/net_prisoner.h
+++ src/net_prisoner.h
@@ -105,11 +105,73 @@ typedef struct {
     int index;
 } connection_t;
 
-void net_server_init();
-void net_server_wait();
-void net_server_game_start();
-void net_server_round_end();
-void net_server_match_end();
+/**
+ * @brief Initialize the server socket and start it in another thread (non-bloking function)
+ * 
+ * in details:
+ * 1 - Initialize variables
+ * 2 - Create the server socket (=bind) with the given ip and port
+ * 3 - Create a thread that will be listening to the socket
+ * 
+ * ! This function isn't blocking, once the thread is started,
+ * ! this function will exit with the thread running in the background
+ * 
+ * @param ip address ip which the server will listen (put '0.0.0.0' to listen everywhere)
+ * @param port network port to listen to
+ */
+void net_server_init(char* ip, int port);
+
+/**
+ * @brief Set the function that the server will trigger when a new client (player)
+ * connect to this server
+ * 
+ * @param f pointer to function (int is the client id)
+ */
+void net_server_set_func_new_client(void (*f)(int));
+
+/**
+ * @brief Set the function that the server will trigger when a client disconnect
+ * 
+ * @param f pointer to function (int is the client id)
+ */
+void net_server_set_func_client_disconnect(void (*f)(int));
+
+/**
+ * @brief Set the function that the server will trigger when a client has click the cooperate button
+ * 
+ * @param f pointer to function (int is the client id; ulong is the time spent answering)
+ */
+void net_server_set_func_cooperate(void (*f)(int, ulong));
+
+/**
+ * @brief Set the function that the server will trigger when a client has click the betray button
+ * 
+ * @param f pointer to function (int is the client id; ulong is the time spent answering)
+ */
+void net_server_set_func_betray(void (*f)(int, ulong));
+
+/**
+ * @brief Tell to specified client that it need to show 'waiting' view
+ * 
+ * @param client client id
+ */
+void net_server_send_screen_waiting(int client);
+
+/**
+ * @brief Tell to specified client that it need to show 'choice' view
+ *  
+ * @param client client id
+ */
+void net_server_send_screen_choice(int client);
+
+/**
+ * @brief send to specified client to switch to 'round end' view
+ * This view should also display on client side if the player has winned or not 
+ * 
+ * @param client client id
+ * @param has_win true if the client has winned, false otherwise
+ */
+void net_server_send_screen_round_end(int client, bool has_win);
 
 //private
 void _net_server_connection_add(connection_t *connection);
